6d60d913cdc5b3b25ad10850bd35eebe8e7414a2
Ratoriku <a1063021545@gmail.com>
2020-01-24 15:57:19 +0800

    Revert "Use TARGET_BOOT_ANIMATION_RES"
    This reverts commit 8960e06f5e2bb16f9af8b65b134947eede55ca03.

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -7,7 +7,8 @@ $(call inherit-product, vendor/xiaomi/msm8974-common/msm8974-common-vendor.mk)
 -include $(LOCAL_PATH)/system_prop.mk
 
 # Boot animation
-TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
 
 # Device uses ultra-high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
