f57c3e604fc0751adc7727681081d0ba832db58e
Ratoriku <a1063021545@gmail.com>
2020-02-11 11:24:49 +0800

    Delete overlay-lineage

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -16,8 +16,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
