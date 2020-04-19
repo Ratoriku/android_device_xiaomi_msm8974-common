e9d53fa2e6efeb1146b5c05090f823434e474f6e
Sultan Alsawaf <sultan@kerneltoast.com>
2020-04-18 23:20:50 -0700

    msm8974-common: Force triple frame buffers
    This yields significantly reduced jitter and doesn't have any measurable
    impact on latency.

--- system_prop.mk
+++ system_prop.mk
@@ -131,4 +131,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.latch_unsignaled=1 \
     debug.sf.disable_backpressure=1 \
-    ro.surface_flinger.supports_background_blur=1
+    ro.surface_flinger.supports_background_blur=1 \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
